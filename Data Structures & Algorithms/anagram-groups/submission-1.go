db606b54ff439fb835078e5a3443714b1b7f0e4a
Ignacio Gunst <ignacioijg@gmail.com>
2026-09-24 11:51:01 -0300

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,19 @@
+func groupAnagrams(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+
+
+	for _, s := range strs {
+		var count [26]int
+		for _,c := range s {
+			count[c - 'a']++
+		}
+		m[count] = append(m[count], s)
+	}
+
+	result := make([][]string,0)
+	for _, v := range m {
+		result = append(result, v)
+	}
+
+	return result
+}
